419ad97058cee4631aafafdb505e86b5f88e638e
Nathan Jones <thenathanjones@gmail.com>
2011-09-17 09:03:54 +0800

    Added the Windows interop methods that will be required, taken from the net

---
+++ NUSB/Interop/InteropKernel32.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using Microsoft.Win32.SafeHandles;
+
+namespace NUSB.Interop
+{
+    internal sealed class InteropKernel32
+    {
+        internal const uint OVERLAPPED = 0x40000000;
+
+        [Flags]
+        internal enum AccessRights : uint
+        {
+            GENERIC_READ = 0x80000000,
+            GENERIC_WRITE = 0x40000000,
+            GENERIC_EXECUTE = 0x20000000,
+            GENERIC_ALL = 0x10000000
+        }
+
+        /// <summary>
+        /// Share Modes.  See MSDN for more information.
+        /// http://msdn.microsoft.com/en-us/library/aa363858.aspx
+        /// </summary>
+        [Flags]
+        internal enum ShareModes : uint
+        {
+            FILE_SHARE_READ = 0x00000001,
+            FILE_SHARE_WRITE = 0x00000002,
+            FILE_SHARE_DELETE = 0x00000004
+        }
+
+
+        /// <summary>
+        /// File Attribute Flags.  See MSDN for more information.
+        /// http://msdn.microsoft.com/en-us/library/aa363858.aspx
+        /// </summary>
+        [Flags]
+        internal enum FileAttributeFlags : uint
+        {
+            FILE_ATTRIBUTE_ARCHIVE = 0x20,
+            FILE_ATTRIBUTE_ENCRYPTED = 0x4000,
+            FILE_ATTRIBUTE_HIDDEN = 0x2,
+            FILE_ATTRIBUTE_NORMAL = 0x80,
+            FILE_ATTRIBUTE_OFFLINE = 0x1000,
+            FILE_ATTRIBUTE_READONLY = 0x1,
+            FILE_ATTRIBUTE_SYSTEM = 0x4,
+            FILE_ATTRIBUTE_TEMPORARY = 0x100,
+            FILE_FLAG_OVERLAPPED = 0x40000000,
+            FILE_FLAG_NO_BUFFERING = 0x20000000
+        }
+
+        internal enum CreationDispositions : int
+        {
+            CREATE_NEW = 1,
+            CREATE_ALWAYS = 2,
+            OPEN_EXISTING = 3,
+            OPEN_ALWAYS = 4,
+            TRUNCATE_EXISTING = 5
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        internal static extern SafeFileHandle CreateFile(
+            string lpFileName,
+            uint dwDesiredAccess,
+            uint dwShareMode,
+            IntPtr lpSecurityAttributes,
+            uint dwCreationDisposition,
+            uint dwFlagsAndAttributes,
+            IntPtr hTemplateFile
+            );
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        internal static extern bool CloseHandle(
+            SafeHandle handle
+            );
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        internal static extern bool CancelIo(
+            SafeHandle handle
+            );
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        unsafe internal static extern bool DeviceIoControl(
+            SafeHandle hDevice,
+            uint dwIoControlCode,
+            byte* lpInBuffer,
+            uint nInBufferSize,
+            byte* lpOutBuffer,
+            uint nOutBufferSize,
+            ref uint lpBytesReturned,
+            NativeOverlapped* lpOverlapped
+            );
+
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        unsafe internal static extern bool WriteFile(
+            SafeHandle hDevice,
+            byte* lpBuffer,
+            uint nNumberOfBytesToWrite,
+            ref uint lpNumberOfBytesWritten,
+            NativeOverlapped* lpOverlapped
+            );
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        unsafe internal static extern bool ReadFile(
+            SafeHandle hDevice,
+            byte* lpBuffer,
+            uint nNumberOfBytesToRead,
+            ref uint lpNumberOfBytesRead,
+            NativeOverlapped* lpOverlapped
+            );
+
+    }
+}
---
+++ NUSB/Interop/InteropSetupAPI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace NUSB.Interop
+{
+    internal sealed class InteropSetupAPI
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr RegisterDeviceNotification(IntPtr hRecipient, IntPtr NotificationFilter, Int32 Flags);
+
+        [DllImport("setupapi.dll", SetLastError = true)]
+        public static extern Int32 SetupDiCreateDeviceInfoList(ref Guid ClassGuid, Int32 hwndParent);
+
+        [DllImport("setupapi.dll", SetLastError = true)]
+        public static extern Int32 SetupDiDestroyDeviceInfoList(IntPtr DeviceInfoSet);
+
+        [DllImport("setupapi.dll", SetLastError = true)]
+        public static extern Boolean SetupDiEnumDeviceInterfaces(IntPtr DeviceInfoSet, IntPtr DeviceInfoData, ref System.Guid InterfaceClassGuid, Int32 MemberIndex, ref USBDeclarations.SP_DEVICE_INTERFACE_DATA DeviceInterfaceData);
+
+        [DllImport("setupapi.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern IntPtr SetupDiGetClassDevs(ref Guid ClassGuid, IntPtr Enumerator, IntPtr hwndParent, Int32 Flags);
+
+        [DllImport("setupapi.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern Boolean SetupDiGetDeviceInterfaceDetail(IntPtr DeviceInfoSet, ref InteropUSB.SP_DEVICE_INTERFACE_DATA DeviceInterfaceData, IntPtr DeviceInterfaceDetailData, Int32 DeviceInterfaceDetailDataSize, ref Int32 RequiredSize, IntPtr DeviceInfoData);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern Boolean UnregisterDeviceNotification(IntPtr Handle);
+    }
+}
