14b0eb126620c83c234d1895e61360d0ad4893ec
Nathan Jones <thenathanjones@gmail.com>
2011-09-18 10:50:46 +0800

    Sample application has some of the colours mapped

--- USBFinderTest/Program.cs
+++ USBFinderTest/Program.cs
@@ -35,17 +35,46 @@ namespace USBFinderTest
             controlBytes[2] = 0x02; // This is the LED byte
             controlBytes[3] = 0xFF;
 
+            while (true)
+            {
+                var entry = Console.ReadLine();
+
+                if (entry.ToLower() == "q")
+                {
+                    break;
+                }
+                if (entry == String.Empty)
+                {
+                    
+                }
+                byte colour;
+                if (Byte.TryParse(entry, out colour))
+                {
+                    controlBytes[2] = colour;
+                    controller.HidSetFeature(controlBytes);
+                }
+                else
+                {
+                    ChangeColour(controller, controlBytes, (LightColour)Enum.Parse(typeof(LightColour), entry));
+                }
+            }
+        }
+
+        private static void ChangeColour(IUSBController controller, byte[] controlBytes, LightColour colour)
+        {
+            controlBytes[2] = (byte)colour;
             controller.HidSetFeature(controlBytes);
+        }
 
-            Console.ReadKey();
-
-            controlBytes[2] = 0x01;
-            controller.HidSetFeature(controlBytes);
-
-            Console.ReadKey();
-
-            controlBytes[2] = 0x00;
-            controller.HidSetFeature(controlBytes);
+        public enum LightColour : byte
+        {
+            Red = 0x02,
+            Green = 0x01,
+            Blue = 0x04,
+            Yellow = 0x03,
+            Aqua = 0x05,
+            Magenta = 0x06,
+            Off = 0x00
         }
     }
 }
